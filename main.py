7ad770a2325aaa742a2330ef456a4e432ade6f1d
MASTER <denkarp123@mail.ru>
2024-03-11 05:03:56 +0500

    New functions: template_response name_of_executor_handler template_importance_handler

---
+++ main.py
@@ -0,0 +1,5 @@
+import tbot
+
+if __name__ == "__main__":
+    tbot.main()
+
--- tbot.py
+++ tbot.py
@@ -20,23 +20,30 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Hello , Im a Master Robot! I am your assistant')
 
 async def template_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    has_context: list = []
+    args: list = context.args
+    args_count: int = len(context.args)
 
-    if len(context.args) > 0:
-        has_context = context.args
+    match args_count:
+        case 0:
+            await update.message.reply_text('This is a /t command. For correct operation give an arguments ["URL", "Name", "Importance", "Time"]')
 
-        link: str = has_context[0]
-        name: str = has_context[1]
-        criticize: str = has_context[2]
-        time: str = has_context[3]
+        case 1:
+            await update.message.reply_text('Need 3 more arguments!')
 
-        text_template: str = '''Ваша заявка зарегистрирована. \nСсылка: {} \nИсполнитель: {} \nКритичность: {} \nВремя решения: {}'''
+        case 2:
+            await update.message.reply_text('Need 2 more arguments!')
 
-        edited_text_template: str = text_template.format(link, name, criticize, time)
+        case 3:
+            await update.message.reply_text('Need 1 more arguments!')
 
-        await update.message.reply_text(edited_text_template)
-    else:
-        await update.message.reply_text('Please enter a incident type : /template [Link] [Name] [Criticality] [Time]')
+        case 4:
+            response: str = template_response(args)
+            await update.message.reply_text(response
+                                            )
+        case _:
+            await update.message.reply_text('You inputted more than 4 arguments or Unexpected Error!')
+
+
 
 
 
@@ -53,36 +60,71 @@ def handle_response(text: str) -> str:
     #Message(Update) text to lowercase for correct response
     processed: str = text.lower()
 
-    #Handle Responses
+    #Template variables
     response: str = 'I do not understand what you wrote'
-    if 'hello' in processed:
-        response = 'Hey there'
-        return response
-    if 'tell' in processed:
-        response = 'I am a Master Robot. I progress everyday and one day i will become an AI!'
-        return response
-    if '1 1 1 1' in processed:
-        response = template_response(processed)
-        return response
+    helpText_list: Final = ['help', 'h', 'hp', 'he']
+
+    #Handle Responses
+    if processed in helpText_list:
+        return 'This Bot works only with commands. Please input a command like [/t or /w] with arguments!'
+
     return response
 
-def template_response(text: str) -> str:
-    processed: str = text.lower()
+def template_response(args: list) -> str:
+    template_of_message: str = '''Ваша заявка зарегистрирована. \nСсылка: {} \nИсполнитель: {} \nКритичность: {} \nВремя решения: {}'''
+    processed_list: list = []
+    result: str = ''
 
-    processed_list: list = processed.split()
+    for i in args:
+        i_lowercase: str = i.lower()
+        processed_list.append(i_lowercase)
 
-    text_template: str = '''Ваша заявка зарегистрирована. \nСсылка: {} \nИсполнитель: {} \nКритичность: {} \nВремя решения: {}'''
+    url: str = processed_list[0]
+    name: str = name_of_executor_handler(processed_list[1])
+    importance: str = template_importance_handler(processed_list[2])
+    time:str = processed_list[3]
 
-    edited_text_template: str = text_template.format(processed_list[0], processed_list[1], processed_list[2], processed_list[3])
+    result = template_of_message.format(url, name, importance, time)
 
-    return edited_text_template
-
-
-
-
-
+    return result
 
 
+def name_of_executor_handler(name: str) -> str:
+    result: str = ''
+    dict_of_executors: dict = {
+        'эш': 'Эдуард Шнабель',
+        'рс': 'Руслан Сабитов',
+        'вг': 'Владимир Гуржий',
+        'аа': 'Алихан Аллаяров',
+        'тм': 'Темирлан Мырзатаев'
+    }
+    for u, n in dict_of_executors.items():
+        if u == name:
+            result = n
+            break
+        else:
+            result = 'None'
+
+    return result
+
+def template_importance_handler(importance: str) -> str:
+    result: str = ''
+    dict_of_importance: dict = {
+        'б': 'Блокирующая',
+        'к': 'Критическая',
+        'з': 'Значительная',
+        'н': 'Незначительная',
+        'т': 'Тривиальная'
+    }
+
+    for u, n in dict_of_importance.items():
+        if u == importance:
+            result = n
+            break
+        else:
+            result = 'None'
+
+    return result
 
 
 
@@ -130,7 +172,7 @@ def error_handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 
-if __name__ == '__main__':
+def main():
     #Application
     print('Application is started')
     app = Application.builder().token(token=token).build()
@@ -148,5 +190,4 @@ if __name__ == '__main__':
     #Polling...
     print('Polling started')
     app.run_polling(3)
-
 
